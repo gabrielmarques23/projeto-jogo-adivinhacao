a2147f93ba2d2e2d7710f4bea652dd7ce71882b3
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-01-11 02:03:21 -0300

    Alguns ajustes na parte visual e ajuste no main.js

--- main.js
+++ main.js
@@ -1,38 +1,50 @@
+// Variáveis da aplicação
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-
-const randomNumber = Math.round(Math.random()*10)
+const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
+//Events
 //Função Callback
-function handleTryClick(event){
+function handleTryClick(event) {
     event.preventDefault() // << Não faça o padrão 
 
     const inputNumber = document.querySelector("#inputNumber")
 
-    if(Number(inputNumber.value) == randomNumber){
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+    if (Number(inputNumber.value) == randomNumber) {
 
         document
-        .querySelector(".screen2 h2")
-        .innerText = `Você acertou em ${xAttempts} tentativas.`
+            .querySelector(".screen2 h2")
+            .innerText = `Você acertou em ${xAttempts} tentativas.`
     }
 
     inputNumber.value = ""
     xAttempts++
-
 }
 
+// Funções Callback
+function handleTryClick() {
+    toggleScreen ()
 
-//Events
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
+    btnTry.addEventListener('click', handleTryClick)
+    btnReset.addEventListener('click', function () {
+        screen1.classList.remove("hide")
+        screen2.classList.add("hide")
 
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function(){
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
+        document.addEventListener('keydown'), function(e){
+            if(e.key == 'Enter'){
+                handleTryClick
+            }
+        }
 
-    xAttempts = 1
-})
+        xAttempts = 1
+    })
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+
+}
