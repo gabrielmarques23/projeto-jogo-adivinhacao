3c7b699827b9b20063979be8d6ffa6f754e9efc4
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-01-11 16:18:48 -0300

    Projeto refatorado e adicionado alfumas melhorias

--- main.js
+++ main.js
@@ -1,50 +1,46 @@
 // Variáveis da aplicação
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-//Events
-//Função Callback
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        // Verifica se a tela 1 está visível antes de chamar handleTryClick
+        if (!screen1.classList.contains("hide")) {
+            handleTryClick(e);
+        }
+    }
+})
+
+//Funções
 function handleTryClick(event) {
     event.preventDefault() // << Não faça o padrão 
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if (Number(inputNumber.value) == randomNumber) {
-
-        document
-            .querySelector(".screen2 h2")
-            .innerText = `Você acertou em ${xAttempts} tentativas.`
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `Você acertou em ${xAttempts} tentativas.`
     }
-
+    
     inputNumber.value = ""
     xAttempts++
 }
 
-// Funções Callback
-function handleTryClick() {
-    toggleScreen ()
-
-    btnTry.addEventListener('click', handleTryClick)
-    btnReset.addEventListener('click', function () {
-        screen1.classList.remove("hide")
-        screen2.classList.add("hide")
-
-        document.addEventListener('keydown'), function(e){
-            if(e.key == 'Enter'){
-                handleTryClick
-            }
-        }
-
-        xAttempts = 1
-    })
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1
 }
 
 function toggleScreen() {
     screen1.classList.toggle("hide")
     screen2.classList.toggle("hide")
-
 }
